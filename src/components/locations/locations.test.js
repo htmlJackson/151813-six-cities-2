2053b0a883a26f063a72b937f16b7d8dc7aa21e7
Evgeny Ber <fiend.jackson@gmail.com>
2019-11-16 04:52:29 +0300

    module5-task1 redux

--- src/components/locations/locations.test.js
+++ src/components/locations/locations.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
-import Locations from "./Locations.jsx";
+import Locations from "./locations.jsx";
 
 it(`Locations correctly renders after relaunch`, () => {
   const tree = renderer.create(<Locations offers={[{}, {}, {}, {}]} citiesList={[`Bobruisk`, `Default city`, `Kishinev`]} city={`Bobruisk`} onCityClick={jest.fn()} />).toJSON();
